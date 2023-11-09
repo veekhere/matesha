8c2b418011fc1e0585e4fd9d9f85c110279a7499
Vee <VKuguenko@croc.ru>
2023-11-09 11:02:36 +0300

    123

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'matesha';
+  a1 = 0;
+  a2 = 0;
+  aa1 = 0;
+  bb1 = 0;
+  aa2 = 0;
+  bb2 = 0;
+
+  plus?: string;
+  minus?: string;
+  proiz?: string;
+  delen?: string;
+
+  constructor(
+    readonly cd: ChangeDetectorRef,
+  ) {}
+
+  opPlus = () => `(${this.a1 + this.a2}, ${this.aa1 + this.aa2}, ${this.bb1 + this.bb2})`;
+  opMinus = () => `(${this.a1 - this.a2}, ${this.aa1 + this.bb2}, ${this.bb1 + this.aa2})`;
+  opPro1 = () => `(${this.a1 * this.a2}, ${this.a1 * this.aa2 + this.a2 * this.aa1}, ${this.a1 * this.bb2 + this.a2 * this.bb1})`;
+  opPro2 = () => `(${this.a1 * this.a2}, ${this.a2 * this.aa1 - this.a1 * this.bb2}, ${this.a2 * this.bb1 - this.a1 * this.aa2})`;
+  opPro3 = () => `(${this.a1 * this.a2}, ${(-this.a2) * this.bb1 - this.a1 * this.bb2}, ${(-this.a2) * this.aa1 - this.a1 * this.aa2})`;
+
+  opPro = () => {
+    if (Number(this.a1) > 0 && Number(this.a2) > 0) {
+      return this.opPro1();
+    } else if (Number(this.a1) < 0 && Number(this.a2) > 0 || Number(this.a1) > 0 && Number(this.a2) < 0) {
+      return this.opPro2();
+    } else if (Number(this.a1) < 0 && Number(this.a2)) {
+      return this.opPro3();
+    } else {
+      return '()';
+    }
+  };
+
+  calc(): void {
+    this.cd.detectChanges();
+  }
 }
